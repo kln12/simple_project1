4a49caa9c165d5b93135e5b70822215ce39ebc3a
Lakshmi Narasimha <lakshminarasimhakandimalla.com>
2023-09-15 19:17:07 +0530

    Add large binary file to

--- main.tf
+++ main.tf
@@ -38,9 +38,15 @@ resource "aws_security_group" "ssh_sg" {
   # Allow SSH access from anywhere (not recommended for production)
   ingress {
     from_port   = 22
-    to_port     = 22
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
   }
 }
 
