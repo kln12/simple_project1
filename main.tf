cfffd6a9c263092baed1ce2d3f8c694e90d89914
Lakshmi Narasimha <lakshminarasimhakandimalla.com>
2023-09-15 19:20:57 +0530

    Add large binary file t

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "ssh_sg" {
   }
   egress {
     from_port   = 22
-    to_port     = 22
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   }
